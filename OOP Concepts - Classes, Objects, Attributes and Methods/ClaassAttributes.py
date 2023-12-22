59e4e8a2797fe1140f429f81988660c06c2c5b38
Shibayan Biswas <114879361+ShibayanBiswas@users.noreply.github.com>
2023-12-22 13:46:47 +0530

    Add files via upload

---
+++ OOP Concepts - Classes, Objects, Attributes and Methods/ClaassAttributes.py
@@ -0,0 +1,45 @@
+# Example 01
+
+class StarCookie:
+    price = 50
+
+    def __init__(self, color, weight):
+        self.color = color
+        self.weight = weight
+
+print(StarCookie.price)
+
+star_cookie1 = StarCookie("Red", 16)
+star_cookie1.price = 55
+print(star_cookie1.color)
+print(star_cookie1.weight)
+print(star_cookie1.price)
+
+star_cookie2 = StarCookie("Blue", 15)
+print(star_cookie2.color)
+print(star_cookie2.weight)
+print(star_cookie2.price)
+
+print(StarCookie.__dict__)
+print(star_cookie1.__dict__)
+print(star_cookie2.__dict__)
+
+
+
+# Example 02
+
+class Youtube:
+    def __init__(self, username, subscribers=0):
+        self.username = username
+        self.subscribers = subscribers
+
+user1 = Youtube("Shibayan")
+user1.likes = 10
+print(user1.username)
+print(user1.subscribers)
+print(user1.likes)
+
+user2 = Youtube("Elshad")
+user2.subscribrs = 5
+print(user2.username)
+print(user2.subscribers)
